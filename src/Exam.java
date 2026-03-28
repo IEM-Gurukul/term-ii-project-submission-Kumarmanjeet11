2bb09ad48574fbea936cfed74c4ac66ba1802823
Kumarmanjeet11 <manjeetkumar71569@gmail.com>
2026-03-28 14:43:16 +0530

    Add files via upload

---
+++ src/Exam.java
@@ -0,0 +1,84 @@
+import java.util.*;
+class Exam{
+    List<Question> question;
+    Map<Question , Integer> answers = new HashMap<>();
+
+    public Exam(List<Question> question){
+     this.question = question;
+}
+
+    public void startExam() {
+        try (Scanner sc = new Scanner(System.in)){
+            for (Question q : question) {
+                q.display();
+                System.out.print("Enter answer:");
+                int ans = sc.nextInt();
+                answers .put(q,ans);
+                }
+            }
+        }
+
+    public int calculateResult() {
+        int score = 0;
+        for (Question q : question) {
+            if (q.checkAnswer(answers.get(q))) {
+                score++;
+            }
+        }
+        return score;
+    }
+}
+class ExamTimer extends Thread {
+    int time;
+
+    public ExamTimer(int time) {
+        this.time = time;
+    }
+    @Override
+    public void run() {
+        try {
+            for (int i = time; i > 0; i--) {
+                System.out.println("Time left: " + i);
+                try {
+                    Thread.sleep(1000);  // Intentional sleep in loop for timer countdown
+                } catch (InterruptedException e) {
+                    System.out.println("Timer interrupted");
+                    break;
+                }
+            }
+            System.out.println("Time's up!");
+        } catch (Exception e) {
+            System.out.println("Timer error: " + e.getMessage());
+        }
+    }
+}
+class Proctoring {
+    public void detectActivity(boolean switchedTab) {
+        if (switchedTab) {
+            System.out.println("Warning: Tab switched!");
+        }
+    }
+}
+
+class TimedExam extends Exam {
+    Proctoring proctoring;
+
+    public TimedExam(List<Question> questions) {
+        super(questions);
+        this.proctoring = new Proctoring();
+    }
+
+    @Override
+    public void startExam() {
+        System.out.println("Starting timed exam...");
+        proctoring.detectActivity(false);
+        super.startExam();
+        ExamTimer timer = new ExamTimer(300);
+        timer.start();
+    }
+}
+
+
+    
+
+
