b249ff760fc9589a211023c3df3293f37185b12d
Kumarmanjeet11 <manjeetkumar71569@gmail.com>
2026-03-28 14:48:47 +0530

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+class Student extends User {
+
+    public Student(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public boolean login() {
+        try{
+            if (username.isEmpty()) {
+            throw new InvalidLoginException("Username cannot be empty");
+        }
+        System.out.println("Login successful");
+    } catch (InvalidLoginException e) {
+        System.out.println(e.getMessage());
+    }
+}
+    @Override
+    public void logout() {
+        System.out.println("Student logged out");
+    }
+   class InvalidLoginException extends Exception {
+    public InvalidLoginException(String msg) {
+        super(msg);
+    }
+   }
+}
